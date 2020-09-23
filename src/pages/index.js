1afae816c848a0d19b2cafc8bd05dedee9ab97ca
Jay T <jaytula@gmail.com>
2020-09-23 14:50:11 -0700

    Move data-item attributes to button

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,30 +33,34 @@ export default () => (
     render={data => (
       <Layout site={data.site}>
         <div className="Catalogue">
-          {data.products.edges.map(({ node: product }) => (
-            <div className="Catalogue__item" key={product.id}>
-              <div
-                className="Product snipcart-add-item"
-                data-item-id={product.id}
-                data-item-price={product.price}
-                data-item-image={product.image.url}
-                data-item-name={product.name}
-                data-item-url={`/`}
-              >
-                <div className="Product__image">
-                  <Img sizes={product.image.sizes} />
-                </div>{" "}
-                <div className="Product__details">
-                  <div className="Product__name">
-                    {product.name}
-                    <div className="Product__price">${product.price}</div>
-                    {product.description && <pre>{product.description}</pre>}
+          {
+            data.products.edges.map(({ node: product }) => (
+              <div className="Catalogue__item" key={product.id}>
+                <div>
+                  <div className="Product__image">
+                    <Img sizes={product.image.sizes} />
+                  </div> <div className="Product__details">
+                    <div className="Product__name">
+                      {product.name}
+                      <div className="Product__price">
+                        ${product.price}
+                      </div>
+                    </div>
+                    <button
+                      className="Product__buy Product snipcart-add-item"
+                      data-item-id={product.id}
+                      data-item-price={product.price}
+                      data-item-image={product.image.url}
+                      data-item-name={product.name}
+                      data-item-url={`/`}
+                    >
+                      Add to cart
+                    </button>
                   </div>
-                  <span className="Product__buy">Buy now</span>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          }
         </div>
       </Layout>
     )}
